102dba3643bc1f44438ac8eaebf474e23ab9554a
Bharath-ctrl <bharathkumar60404@gmail.com>
2022-03-10 22:22:34 +0530

    10/3/22_practice

---
+++ python practice/encapsulation.py
@@ -0,0 +1,17 @@
+#dataEncapsulation
+class School:
+    def __init__(self):
+        self.__marks=80     #__marks means private marks....in python we use __ instead of private
+    def viewMarks(self):
+        print(self.__marks)
+    def Teacher(self,marks):
+        self.__marks=marks
+
+x= School()
+x.viewMarks()
+
+x.__marks=100       #cannot change datat from outside
+x.viewMarks()
+
+x.Teacher(130)
+x.viewMarks()
