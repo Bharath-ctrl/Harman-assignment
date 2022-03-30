6f8b365df31c51522aaa577048542e630e598cba
Bharath-ctrl <63045800+Bharath-ctrl@users.noreply.github.com>
2022-03-30 10:52:01 +0530

    Add files via upload

---
+++ python practice/March30Selenium/test_facebook.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver=webdriver.Chrome(ChromeDriverManager().install())
+print("Testcase started")
+driver.maximize_window()
+driver.get("https://www.facebook.com/")
+driver.find_element_by_name("email").send_keys("8660161452")
+time.sleep(1)
+driver.find_element_by_name("pass").send_keys("charlie_X95")
+time.sleep(5)
+
+driver.find_element_by_name("login").send_keys(Keys.ENTER)
+time.sleep(10)
+
+driver.close()
+print("Testcase completed")
+
